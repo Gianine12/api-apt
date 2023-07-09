ce51713d3431af8a4894dff5ae2446147532e3d7
Gianine DataType <gianine.rosa@datatype.com.br>
2023-07-09 18:10:33 -0300

    projeto clonado

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 const VisitanteController = require('./Controller/VisitanteController');
-
+//gianine
 routes.post('/visitante', VisitanteController.createVisitante);
 routes.get('/visitantes', VisitanteController.getVisitante);
 
